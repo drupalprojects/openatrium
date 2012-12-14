d8c7bb32e8aff40ec8f088f0994f25e0ffd5f016
Peter Cho <pcho@phase2technology.com>
2012-12-14 23:22:33 +0000

    Added the dashboard widget module

---
+++ modules/openatrium2/oa2_dashboard/oa2_toolbar.tpl.php
@@ -0,0 +1,15 @@
+<ul id="oa2_toolbar">
+	<li><?php print $home_label; ?></li>
+	<li class="oa2_toolbar_separator"><?php print $oa2_toolbar_separator; ?></li>
+	<li><?php print $groups_label; ?>
+		<ul>
+		  <li><?php print $groups_recent; ?></li>
+		</ul>
+	</li>
+	<li class="oa2_toolbar_separator"><?php print $oa2_toolbar_separator; ?></li>
+	<li><?php print $sections_label; ?>
+		<ul>
+		  <li><?php print $sections_list; ?></li>
+		</ul>
+  </li>
+</ul>
