8852af2989948a25589d25f984f89f0e32b9dab4
mpotter <mpotter@phase2technology.com>
2012-12-26 20:57:16 +0000

    More removals of v2

--- modules/openatrium/oa_dashboard/oa_toolbar.tpl.php
+++ modules/openatrium/oa_dashboard/oa_toolbar.tpl.php
@@ -1,12 +1,12 @@
-<ul id="oa2_toolbar">
+<ul id="oa_toolbar">
   <li><?php print $home_label; ?></li>
-  <li class="oa2_toolbar_separator"><?php print $oa2_toolbar_separator; ?></li>
+  <li class="oa_toolbar_separator"><?php print $oa_toolbar_separator; ?></li>
   <li><?php print $groups_label; ?>
     <ul>
       <li><?php print $groups_recent; ?></li>
     </ul>
   </li>
-  <li class="oa2_toolbar_separator"><?php print $oa2_toolbar_separator; ?></li>
+  <li class="oa_toolbar_separator"><?php print $oa_toolbar_separator; ?></li>
   <li><?php print $sections_label; ?>
     <ul>
       <li><?php print $sections_list; ?></li>
