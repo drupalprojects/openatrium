9624d581142f5b306fd10d0cde7520e6a41467e7
mpotter <mpotter@phase2technology.com>
2012-12-27 21:13:24 +0000

    Added Radix and Bootstrap theme

--- modules/openatrium/oa_dashboard/oa_toolbar.tpl.php
+++ modules/openatrium/oa_dashboard/oa_toolbar.tpl.php
@@ -1,15 +1,20 @@
-<ul id="oa_toolbar">
-  <li><?php print $home_label; ?></li>
-  <li class="oa_toolbar_separator"><?php print $oa_toolbar_separator; ?></li>
-  <li><?php print $groups_label; ?>
-    <ul>
+<div id="oa_toolbar">
+  <span class="dropdown">
+    <a class="dropdown-toggle" data-toggle="dropdown" href="#"><i class="icon-home"></i></a>
+  </span>
+  <span class="oa_toolbar_separator"><?php print $oa_toolbar_separator; ?></span>
+  <span class="dropdown">
+    <a class="dropdown-toggle btn btn-large btn-info" id="group-dropdown" data-toggle="dropdown" href="#"><?php print $groups_label; ?>&nbsp;<b class="caret"></b></a>
+    <ul class="dropdown-menu" role="menu" aria-labelledby="group-dropdown">
       <li><?php print $groups_recent; ?></li>
     </ul>
-  </li>
-  <li class="oa_toolbar_separator"><?php print $oa_toolbar_separator; ?></li>
-  <li><?php print $sections_label; ?>
-    <ul>
+  </span>
+  <span class="oa_toolbar_separator"><?php print $oa_toolbar_separator; ?></span>
+  <span class="dropdown">
+    <a class="dropdown-toggle btn btn-large btn-info" id="section-dropdown" data-toggle="dropdown" href="#"><?php print $sections_label; ?>&nbsp;<b class="caret"></b></a>
+    <ul class="dropdown-menu" role="menu" aria-labelledby="section-dropdown">
+      <li><ul><li><?php print $sections_main; ?></li></ul></li>
       <li><?php print $sections_list; ?></li>
     </ul>
-  </li>
-</ul>
+  </span>
+</div>
