561631cb1f3830af820b6893cc28ce5173d19a10
mpotter <mpotter@phase2technology.com>
2012-12-27 21:46:40 +0000

    Button colors

--- modules/openatrium/oa_dashboard/oa_toolbar.tpl.php
+++ modules/openatrium/oa_dashboard/oa_toolbar.tpl.php
@@ -4,14 +4,14 @@
   </span>
   <span class="oa_toolbar_separator"><?php print $oa_toolbar_separator; ?></span>
   <span class="dropdown">
-    <a class="dropdown-toggle btn btn-large btn-info" id="group-dropdown" data-toggle="dropdown" href="#"><?php print $groups_label; ?>&nbsp;<b class="caret"></b></a>
+    <a class="dropdown-toggle btn btn-large btn-success" id="group-dropdown" data-toggle="dropdown" href="#"><?php print $groups_label; ?>&nbsp;<b class="caret"></b></a>
     <ul class="dropdown-menu" role="menu" aria-labelledby="group-dropdown">
       <li><?php print $groups_recent; ?></li>
     </ul>
   </span>
   <span class="oa_toolbar_separator"><?php print $oa_toolbar_separator; ?></span>
   <span class="dropdown">
-    <a class="dropdown-toggle btn btn-large btn-info" id="section-dropdown" data-toggle="dropdown" href="#"><?php print $sections_label; ?>&nbsp;<b class="caret"></b></a>
+    <a class="dropdown-toggle btn btn-large btn-success" id="section-dropdown" data-toggle="dropdown" href="#"><?php print $sections_label; ?>&nbsp;<b class="caret"></b></a>
     <ul class="dropdown-menu" role="menu" aria-labelledby="section-dropdown">
       <li><ul><li><?php print $sections_main; ?></li></ul></li>
       <li><?php print $sections_list; ?></li>
