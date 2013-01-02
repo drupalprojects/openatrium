7e82549bebb7e6d1ba654740b7627522dc6139f4
mpotter <mpotter@phase2technology.com>
2013-01-02 22:28:17 +0000

    Added Group and Section popup

--- modules/openatrium/oa_dashboard/oa_toolbar.tpl.php
+++ modules/openatrium/oa_dashboard/oa_toolbar.tpl.php
@@ -7,15 +7,15 @@
       <?php print $groups_label; ?>
       <a class="dropdown-toggle btn btn-large btn-success" id="group-dropdown" data-toggle="dropdown" href="#"><b class="caret"></b></a>
       <ul class="dropdown-menu" role="menu" aria-labelledby="group-dropdown">
-        <li><?php print $groups_recent; ?></li>
+        <li class="dropdown-column"><?php print $groups_favorite; ?></li>
+        <li class="dropdown-column"><?php print $groups_recent; ?></li>
       </ul>
     </div>
   <div class="oa-toolbar-separator"><?php print $oa_toolbar_separator; ?></div>
   <div class="dropdown btn-group">
     <a class="dropdown-toggle btn btn-large btn-success" id="section-dropdown" data-toggle="dropdown" href="#"><?php print $sections_label; ?>&nbsp;<b class="caret"></b></a>
     <ul class="dropdown-menu" role="menu" aria-labelledby="section-dropdown">
-      <li><ul><li><?php print $sections_main; ?></li></ul></li>
-      <li><?php print $sections_list; ?></li>
+      <li class="dropdown-column"><?php print $sections_list; ?></li>
     </ul>
   </div>
 </div>
