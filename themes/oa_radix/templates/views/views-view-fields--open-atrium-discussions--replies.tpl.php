81ffc29f5889a464dc3960eba1ff298b696a2bae
mpotter <mpotter@phase2technology.com>
2013-02-14 22:57:12 +0000

    OA-229, Created templates for Reply and updated layout

--- themes/oa_radix/templates/views/views-view-fields--open-atrium-discussions--replies.tpl.php
+++ themes/oa_radix/templates/views/views-view-fields--open-atrium-discussions--replies.tpl.php
@@ -25,35 +25,25 @@
  */
 ?>
 <div class='oa-list well'>
-  <?php if ($display != 'user_activity'): ?>
-    <div class='pull-right user-badge'>
-      <?php print $name; ?>
-      <?php print $field_user_picture; ?>
-    </div>
-  <?php endif; ?>
-  <div class='oa-list-category pull-right'>
-    <?php if ($display != 'group_activity'): ?>
-      <?php print $og_group_ref; ?><br \>
-    <?php endif; ?>
-    <?php print $oa_section_ref; ?>
+  <div class='pull-right user-badge'>
+    <?php print $name; ?>
+    <?php print $field_user_picture; ?>
   </div>
   <div class='oa-list-header'>
-    <?php print $timestamp; ?>
-    <?php print $name; ?>
-    <?php print $type; ?>
-    <?php print $type_1; ?>:
+    <?php print $counter; ?>
+    <?php print $created; ?>
     <?php print $title; ?>
   </div>
-  <div class="accordion" id="oa-river-accordion<?php print $index; ?>">
+  <div class="accordion" id="oa-reply-accordion<?php print $index; ?>">
     <div>
       <div class="accordion-heading">
-        <a class="accordion-toggle" data-toggle="collapse" data-parent="#oa-river-accordion<?php print $index; ?>" href="#oa-river-body<?php print $index; ?>">
-          <i class="icon-plus" vertical-align="middle"></i>&nbsp;<?php print $body; ?>...
+        <a class="accordion-toggle" data-toggle="collapse" data-parent="#oa-reply-accordion<?php print $index; ?>" href="#oa-reply-body<?php print $index; ?>">
+          <i class="icon-plus" vertical-align="middle"></i>&nbsp;<?php print $body_1; ?>...
         </a>
       </div>
-      <div id="oa-river-body<?php print $index; ?>" class="accordion-body collapse">
+      <div id="oa-reply-body<?php print $index; ?>" class="accordion-body collapse">
         <div class="accordion-inner">
-          <?php print $rendered_entity; ?>
+          <?php print $body; ?>
         </div>
       </div>
     </div>
