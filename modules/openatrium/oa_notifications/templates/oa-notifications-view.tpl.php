a502dd1c00a4fd1214e9896e2fa71ed377e1ae74
Frank Febbraro <frank@phase2technology.com>
2013-02-20 22:23:10 +0000

    removed the well style and added some classes

--- modules/openatrium/oa_notifications/templates/oa-notifications-view.tpl.php
+++ modules/openatrium/oa_notifications/templates/oa-notifications-view.tpl.php
@@ -1,20 +1,20 @@
-<div class="well well-large">
-    <div class="">
-      <h5><?php print t('Organizations'); ?></h5>
-      <?php foreach ($organization as $o): ?>
-        <span class="label label-info"><?php print $o; ?></span>
-      <?php endforeach; ?>
-    </div>
-    <div class="">
-      <h5><?php print t('Teams'); ?></h5>
-      <?php foreach ($team as $t): ?>
-        <span class="label label-info"><?php print $t; ?></span>
-      <?php endforeach; ?>
-    </div>
-    <div class="">
-      <h5><?php print t('Users'); ?></h5>
-      <?php foreach ($user as $u): ?>
-        <span class="label label-info"><?php print $u; ?></span>
-      <?php endforeach; ?>
-    </div>
+<div class="notifications">
+  <div class="notification-group">
+    <h5><?php print t('Organizations'); ?></h5>
+    <?php foreach ($organization as $o): ?>
+      <span class="label label-info"><?php print $o; ?></span>
+    <?php endforeach; ?>
+  </div>
+  <div class="notification-group">
+    <h5><?php print t('Teams'); ?></h5>
+    <?php foreach ($team as $t): ?>
+      <span class="label label-info"><?php print $t; ?></span>
+    <?php endforeach; ?>
+  </div>
+  <div class="notification-group">
+    <h5><?php print t('Users'); ?></h5>
+    <?php foreach ($user as $u): ?>
+      <span class="label label-info"><?php print $u; ?></span>
+    <?php endforeach; ?>
+  </div>
 </div>
