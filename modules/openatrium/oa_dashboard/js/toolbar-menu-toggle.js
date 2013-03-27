675311498baebc8e1d901e1eb97891feea24e2f1
Will O'Beirne <wobeirne@phase2technology.com>
2013-03-27 15:43:46 +0000

    Fixed issue with navbar tray being cut off by OA toolbar. Also fixed issue where menu button wouldnt open tray after AJAX.

--- modules/openatrium/oa_dashboard/js/toolbar-menu-toggle.js
+++ modules/openatrium/oa_dashboard/js/toolbar-menu-toggle.js
@@ -6,22 +6,24 @@
 
   Drupal.behaviors.toolbarMenuButton = {
     attach: function(context, settings) {
-      var tray = $('#navbar .navbar-tray');
-      var button = $('#toolbar-menu-button');
-      var navbar = $('#oa-navbar');
+      var $tray = $('#navbar .navbar-tray');
+      var $button = $('#toolbar-menu-button');
+      var $navbar = $('#oa-navbar');
 
-      if (button.length) {
+      if ($button.length) {
         // if using the button in toolbar, hide the navbar module's nav bar
         $('#navbar .navbar-bar').hide();
       }
-      if (tray.length && button.length) {
-        button.click(function(e) {
+      if ($tray.length && $button.length) {
+        $button.unbind('click');
+        $button.click(function(e) {
           $('body').toggleClass('menu-tray-open');
-          tray.toggleClass('active');
+          $tray.toggleClass('active');
 
-          // When it's stickied, we need to manually shrink the navbar width.
+          // When it's stickied, we need to push the tray down.
           if (settings.oa_toolbar_sticky == 1) {
-            navbar.css('margin-left', tray.hasClass('active') ? tray.width() : 0);
+            // Use attr for !important to override radix.
+            $tray.attr('style', 'top: ' + $navbar.height() + 'px !important');
           }
         });
       }
