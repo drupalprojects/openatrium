a1f97c6fe260eca643f37ad068c1d445ee0087ac
Will O'Beirne <wobeirne@phase2technology.com>
2013-03-27 15:17:31 +0000

    Tweaked the secondary tab styles so that bootstraps base tab styles arent effected.

--- themes/oa_radix/templates/page.tpl.php
+++ themes/oa_radix/templates/page.tpl.php
@@ -84,7 +84,7 @@
           <?php print render($title_prefix); ?>
           <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
           <?php print render($title_suffix); ?>
-          <?php if (!empty($tabs['#primary']) || !empty($tabs['#secondary'])): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
+          <?php if (!empty($tabs['#primary']) || !empty($tabs['#secondary'])): ?><div class="tabs main-tabs"><?php print render($tabs); ?></div><?php endif; ?>
           <?php print render($page['help']); ?>
           <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
           <?php print render($page['content']); ?>
