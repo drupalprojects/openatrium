ce29c1951c125bf8a49b12769723ff626a2d57cd
mpotter <mpotter@phase2technology.com>
2013-04-22 00:01:38 +0000

    OA-94, Add membership admin functions to member widgets

--- modules/features/oa_core/templates/oa-core-members-widget.tpl.php
+++ modules/features/oa_core/templates/oa-core-members-widget.tpl.php
@@ -18,10 +18,26 @@
 <div class='clearfix well well-small owners'>
   <h5>Space owners</h5>
   <?php foreach ($owners as $owner): ?>
-    <div class='pull-left user-badge'>
-      <?php print $owner['picture']; ?>
-      <?php print $owner['title']; ?>
-    </div>
+    <?php if (empty($links['add-admin'])): ?>
+      <div class='pull-left user-badge'>
+        <?php print $owner['picture']; ?>
+        <?php print $owner['title']; ?>
+      </div>
+    <?php else: ?>
+      <div class="dropdown oa-dropdown btn-group">
+        <div class="dropdown-toggle btn pull-left user-badge" data-toggle="dropdown">
+          <?php print $owner['picture']; ?>
+          <?php print $owner['title']; ?>
+          <i class='icon-chevron-down'></i>
+        </div>
+        <div class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu">
+          <ul>
+            <li><?php print l(t('Dashboard'), $links['dashboard'] . $owner['uid']); ?></li>
+            <li><?php print l(t('Remove as Admin'), $links['remove-admin'] . $owner['uid']); ?></li>
+          </ul>
+        </div>
+      </div>
+    <?php endif; ?>
   <?php endforeach; ?>
 </div>
 <?php endif; ?>
@@ -40,10 +56,27 @@
 <?php if (!empty($members)): ?>
 <div class='clearfix members'>
   <?php foreach ($members as $member): ?>
-    <div class='oa-member-list user-badge no-border pull-left'>
-      <?php print $member['picture']; ?>
-      <?php print $member['title']; ?>
-    </div>
+    <?php if (empty($links['add-admin'])): ?>
+      <div class='pull-left user-badge'>
+        <?php print $member['picture']; ?>
+        <?php print $member['title']; ?>
+      </div>
+    <?php else: ?>
+      <div class="dropdown oa-dropdown btn-group">
+        <div class="dropdown-toggle btn pull-left user-badge" data-toggle="dropdown">
+          <?php print $member['picture']; ?>
+          <?php print $member['title']; ?>
+          <i class='icon-chevron-down'></i>
+        </div>
+        <div class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu">
+          <ul>
+            <li><?php print l(t('Dashboard'), $links['dashboard'] . $member['uid']); ?></li>
+            <li><?php print l(t('Remove from space'), $links['remove'] . $member['uid']); ?></li>
+            <li><?php print l(t('Add as Admin'), $links['add-admin'] . $member['uid']); ?></li>
+          </ul>
+        </div>
+      </div>
+    <?php endif; ?>
   <?php endforeach; ?>
   <div class="clear-both more">
     <?php print $see_all_link; ?>
