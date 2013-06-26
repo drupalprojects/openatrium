85b684dfb5c5f7166b7836a1ed73d86d55dba420
mpotter <mpotter@phase2technology.com>
2013-06-26 20:21:35 +0000

    Add children spaces to oa_breadcrumb widget

--- modules/openatrium/oa_dashboard/oa_breadcrumb.tpl.php
+++ modules/openatrium/oa_dashboard/oa_breadcrumb.tpl.php
@@ -9,18 +9,28 @@
         <a href="<?php print $parent_url; ?>" class="btn <?php print $oa_toolbar_btn_class; ?>"><?php print $parent_label; ?></a>
       </li>
       <?php else: ?>
-      <li class="dropdown btn-group <?php if (!empty($parent_list)) { print 'dropdown';}?>">
+      <li class="dropdown btn-group dropdown">
         <a href="<?php print $parent_url; ?>" class="btn <?php print $oa_toolbar_btn_class; ?>"><?php print $parent_label; ?></a>
-        <a class="dropdown-toggle btn <?php print $oa_toolbar_btn_class; ?>" id="section-dropdown" data-toggle="dropdown" href="#"><b class="caret"></b></a>
-        <ul class="dropdown-menu" role="menu" aria-labelledby="section-dropdown">
+        <a class="dropdown-toggle btn <?php print $oa_toolbar_btn_class; ?>" id="parent-dropdown" data-toggle="dropdown" href="#"><b class="caret"></b></a>
+        <ul class="dropdown-menu" role="menu" aria-labelledby="parent-dropdown">
           <li class="dropdown-column"><?php print $parent_list; ?></li>
         </ul>
       </li>
       <?php endif; ?>
     <?php endif; ?>
-    <li class="btn-group">
-      <a href="<?php print $space_url; ?>" class="btn <?php print $oa_toolbar_btn_class; ?>"><?php print $space_label; ?></a>
-    </li>
+    <?php if (empty($spaces_recent)): ?>
+      <li class="btn-group">
+        <a href="<?php print $space_url; ?>" class="btn <?php print $oa_toolbar_btn_class; ?>"><?php print $space_label; ?></a>
+      </li>
+    <?php else: ?>
+      <li class="dropdown btn-group dropdown">
+        <a href="<?php print $space_url; ?>" class="btn <?php print $oa_toolbar_btn_class; ?>"><?php print $space_label; ?></a>
+        <a class="dropdown-toggle btn <?php print $oa_toolbar_btn_class; ?>" id="space-dropdown" data-toggle="dropdown" href="#"><b class="caret"></b></a>
+        <ul class="dropdown-menu" role="menu" aria-labelledby="space-dropdown">
+          <li class="dropdown-column"><?php print $spaces_recent; ?></li>
+        </ul>
+      </li>
+    <?php endif; ?>
     <?php if (!empty($section_label)): ?>
     <li class="dropdown btn-group">
       <a href="<?php print $section_url; ?>" class="btn <?php print $oa_toolbar_btn_class; ?>"><?php print $section_label; ?></a>
