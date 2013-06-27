520e97771b8175cdad6532038189d79d5aa636f5
mpotter <mpotter@phase2technology.com>
2013-06-27 20:10:02 +0000

    Added Visibility widget, adjusted css for widgets

--- modules/features/oa_favorites/templates/oa-favorites-favorites-button.tpl.php
+++ modules/features/oa_favorites/templates/oa-favorites-favorites-button.tpl.php
@@ -8,7 +8,9 @@
 ?>
 <ul id="oa_favorites">
   <li class="dropdown btn-group">
-    <a class="dropdown-toggle btn" id="favorites-dropdown" data-toggle="dropdown" href="#"><b class="caret"></b></a>
+    <a class="dropdown-toggle btn <?php print $oa_toolbar_btn_class; ?>" id="favorites-dropdown" data-toggle="dropdown" href="#" title="<?php print $title?>">
+      <i class="icon-star"></i>
+    </a>
     <ul class="dropdown-menu" role="menu" aria-labelledby="space-dropdown">
       <li class="dropdown-column"><?php print $spaces_favorite; ?></li>
     </ul>
