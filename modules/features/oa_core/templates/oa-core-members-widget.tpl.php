1f6bec3f56b7420286d78ccfdde76c2a22367a6f
mpotter <mpotter@phase2technology.com>
2013-07-03 20:20:17 +0000

    Refactor members widget to allow tabs and handle pending members

--- modules/features/oa_core/templates/oa-core-members-widget.tpl.php
+++ modules/features/oa_core/templates/oa-core-members-widget.tpl.php
@@ -3,107 +3,86 @@
  * @file
  * Template that provides the view for the space members widget.
  *
- * $owners - Array of owners.
- *   'title'    - Link to the owner user.
- *   'picture'  - Rendered image of the user.
- * $teams - Array of teams.
- *   'title'    - Link to the team node.
- * $users - Array of users.
- *   'title'    - Link to the owner user.
- *   'picture'  - Rendered image of the user.
+ * $tabs[$category] = array
+ *   'title' - title of category
+ *   'items' - array of users
+ *   'links' - array of links
+ *
+ * array of users = array
+ *   'title' - name of user
+ *   'picture' - user picture
+ *   'uid' - user id
+ *
+ * array of links = array
+ *   'title' - link text
+ *   'url' - link url.  user id will be appended to this url
  */
 ?>
 
-<?php
-  $well = (!empty($title)) ? ' well well-small' : '';
-?>
 <div id="oa-core-messages"></div>
-<?php if (!empty($parents)): ?>
-<div class='clearfix <?php print $well;?> parents'>
-  <?php if (!empty($title['parents'])):?>
-    <h5><?php print $title['parents']?></h5>
-  <?php endif;?>
-  <?php foreach ($parents as $parent): ?>
-    <div class='pull-left parent label'>
-      <?php print $parent['title']; ?>
-    </div>
-  <?php endforeach; ?>
-</div>
-<?php endif; ?>
 
-<?php if (!empty($owners)): ?>
-<div class='clearfix <?php print $well;?> owners'>
-  <?php if (!empty($title['owners'])):?>
-    <h5><?php print $title['owners']?></h5>
-  <?php endif;?>
-  <?php foreach ($owners as $owner): ?>
-    <?php if (empty($links['remove-admin']) || empty($owner['uid'])): ?>
-      <div class='pull-left user-badge'>
-        <?php print $owner['picture']; ?>
-        <?php print $owner['title']; ?>
-      </div>
-    <?php else: ?>
-      <div class="dropdown oa-dropdown btn-group">
-        <div class="dropdown-toggle btn pull-left user-badge" data-toggle="dropdown">
-          <?php print $owner['picture']; ?>
-          <?php print $owner['title']; ?>
-          <i class='icon-chevron-down'></i>
-        </div>
-        <div class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu">
-          <ul>
-            <li><?php print l(t('Dashboard'), $links['dashboard'] . $owner['uid']); ?></li>
-            <li><?php print l(t('Remove as Admin'), $links['remove-admin'] . $owner['uid'] . '/nojs', $owner['options']); ?></li>
-          </ul>
-        </div>
+<?php if ($show_as_tabs): ?>
+  <ul class="nav nav-tabs">
+    <?php foreach ($categories as $cat): ?>
+      <?php if (!empty($tabs[$cat]['items'])): ?>
+      <li class="tab-<?php print $cat; ?> <?php if ($cat == $active) {print 'active';}?>"><a href="#tab-<?php print $cat?>" data-toggle="tab">
+        <?php print $tabs[$cat]['caption']; ?>
+      </a></li>
+      <?php endif; ?>
+    <?php endforeach; ?>
+  </ul>
+<?php endif; ?>
+<div class="tab-content">
+<?php foreach ($categories as $cat): ?>
+  <?php if (!empty($tabs[$cat]['items'])): ?>
+    <?php if ($show_as_tabs): ?>
+      <div class="tab-pane <?php if ($cat == $active) {print 'active';}?>" id="<?php print 'tab-'.$cat; ?>">
+    <?php endif; ?>
+    <?php
+      $tab = $tabs[$cat];
+      $well = (!empty($tab['title'])) ? ' well well-small' : '';
+    ?>
+    <div class='clearfix <?php print $well . ' ' . $cat;?>'>
+      <?php if (!empty($tab['title'])):?>
+        <h5><?php print $tab['title']?></h5>
+      <?php endif;?>
+      <?php foreach ($tab['items'] as $item): ?>
+        <?php if (empty($tab['links'])): ?>
+          <div class='pull-left label'>
+            <?php print $item['title']; ?>
+          </div>
+        <?php elseif (empty($item['uid'])): ?>
+          <div class='pull-left user-badge'>
+            <?php print $item['picture']; ?>
+            <?php print $item['title']; ?>
+          </div>
+        <?php else: ?>
+          <div class="dropdown oa-dropdown btn-group">
+            <div class="dropdown-toggle btn pull-left user-badge" data-toggle="dropdown">
+              <?php print $item['picture']; ?>
+              <?php print $item['title']; ?>
+              <i class='icon-chevron-down'></i>
+            </div>
+            <div class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu">
+              <ul>
+                <?php foreach ($tab['links'] as $lid): ?>
+                  <?php if (!empty($links[$lid]['url'])): ?>
+                  <li>
+                    <?php
+                      $url = str_replace('%uid', $item['uid'], $links[$lid]['url']);
+                      print l($links[$lid]['title'], $url, $item['options']); ?>
+                  </li>
+                  <?php endif; ?>
+                <?php endforeach; ?>
+              </ul>
+            </div>
+          </div>
+        <?php endif; ?>
+      <?php endforeach; ?>
+    </div>
+    <?php if ($show_as_tabs): ?>
       </div>
     <?php endif; ?>
-  <?php endforeach; ?>
-</div>
-<?php endif; ?>
-
-<?php if (!empty($teams)): ?>
-<div class='clearfix <?php print $well;?> teams'>
-  <?php if (!empty($title['teams'])):?>
-    <h5><?php print $title['teams']?></h5>
   <?php endif; ?>
-  <?php foreach ($teams as $team): ?>
-    <div class='pull-left team label'>
-      <?php print $team['title']; ?>
-    </div>
-  <?php endforeach; ?>
+<?php endforeach; ?>
 </div>
-<?php endif; ?>
-
-<?php if (!empty($members)): ?>
-<div class='clearfix members'>
-  <?php foreach ($members as $member): ?>
-    <?php if (empty($links['add-admin']) || empty($member['uid'])): ?>
-      <div class='pull-left user-badge'>
-        <?php print $member['picture']; ?>
-        <?php print $member['title']; ?>
-      </div>
-    <?php else: ?>
-      <div class="dropdown oa-dropdown btn-group">
-        <div class="dropdown-toggle btn pull-left user-badge" data-toggle="dropdown">
-          <?php print $member['picture']; ?>
-          <?php print $member['title']; ?>
-          <i class='icon-chevron-down'></i>
-        </div>
-        <div class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu">
-          <ul>
-            <li><?php print l(t('Dashboard'), $links['dashboard'] . $member['uid']); ?></li>
-            <li><?php print l(t('Remove from @label', array('@label' => $title['members'])), $links['remove'] . $member['uid'] . '/nojs', $member['options']); ?></li>
-            <?php if (!empty($links['remove-child'])): ?>
-              <li><?php print l(t('Remove from @label and any children spaces', array('@label' => $title['members'])), $links['remove-child'] . $member['uid'] . '/nojs', $member['options']); ?></li>
-            <?php endif;?>
-            <li><?php print l(t('Add as Admin'), $links['add-admin'] . $member['uid'] . '/nojs', $member['options']); ?></li>
-          </ul>
-        </div>
-      </div>
-    <?php endif; ?>
-  <?php endforeach; ?>
-  <div class="clear-both more">
-    <?php print $see_all_link; ?>
-  </div>
-</div>
-<?php endif; ?>
