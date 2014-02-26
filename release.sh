a6e4077b9ee57cb3a3453e50ee10218f6a48d006
mpotter <mpotter@phase2technology.com>
2014-02-26 20:39:21 +0000

    update release.sh script

--- release.sh
+++ release.sh
@@ -11,8 +11,10 @@ DRUPAL_VERSION="7.x-"$VERSION
 # first, patch the openatrium.info file with version
 echo "Patching version info..."
 sed -i "s/version = .*/version = \"$DRUPAL_VERSION\"/" openatrium.info
+git commit -a -m "Updating version info"
+git push origin 7.x.2.x
 echo "Creating tag...$DRUPAL_VERSION"
 git checkout 7.x-2.x
-git tag $DRUPAL_VERSION
-git push origin tag $DRUPAL_VERSION
+#git tag $DRUPAL_VERSION
+#git push origin tag $DRUPAL_VERSION
 echo "Release complete!"
